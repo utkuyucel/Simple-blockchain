60a866e99bb98823194af406a76e702078ba9764
Utku Yücel <40201124+utkuyucel@users.noreply.github.com>
2023-02-15 20:43:30 +0300

    Update run.py
    Transfer validation and transactions has added.

--- run.py
+++ run.py
@@ -1,46 +1,110 @@
 import hashlib
 import datetime
 
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+
+    def is_valid(self):
+        if self.amount <= 0:
+            return False
+        return True
+
+
 class Block:
-    def __init__(self, data, previous_hash):
+    def __init__(self, transactions, previous_hash):
         self.timestamp = datetime.datetime.now()
-        self.data = data
+        self.transactions = transactions
         self.previous_hash = previous_hash
+        self.nonce = 0
         self.hash = self.calc_hash()
 
     def calc_hash(self):
         sha = hashlib.sha256()
 
-        # Hash the block's data and the previous block's hash
-        hash_str = str(self.data).encode('utf-8') + str(self.previous_hash).encode('utf-8')
+        # Hash the block's transaction data, previous block's hash, and nonce
+        hash_str = str([t.__dict__ for t in self.transactions]).encode('utf-8') + \
+                   str(self.previous_hash).encode('utf-8') + str(self.nonce).encode('utf-8')
 
         sha.update(hash_str)
 
         return sha.hexdigest()
 
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calc_hash()
+
+        print("Block mined: ", self.hash)
+
+    def has_valid_transactions(self):
+        for transaction in self.transactions:
+            if not transaction.is_valid():
+                return False
+        return True
+
+
 class Blockchain:
     def __init__(self):
-        self.chain = [Block("Genesis Block", None)]
+        self.chain = [Block([], None)]
+        self.difficulty = 2
 
-    def add_block(self, data):
+    def add_block(self, transactions):
         previous_hash = self.chain[-1].hash
-        new_block = Block(data, previous_hash)
+        new_block = Block(transactions, previous_hash)
+        new_block.mine_block(self.difficulty)
         self.chain.append(new_block)
 
     def print_chain(self):
         for block in self.chain:
             print("Timestamp: ", block.timestamp)
-            print("Data: ", block.data)
+            print("Transactions: ", [t.__dict__ for t in block.transactions])
             print("Previous Hash: ", block.previous_hash)
             print("Hash: ", block.hash)
             print()
 
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if current_block.hash != current_block.calc_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+            if not current_block.has_valid_transactions():
+                return False
+
+        return True
+
 
 if __name__ == "__main__":
-  blockchain = Blockchain()
+    blockchain = Blockchain()
 
-  blockchain.add_block("This is block 1")
-  blockchain.add_block("This is block 2")
-  blockchain.add_block("This is block 3")
+    # create some transactions
+    tx1 = Transaction("Alice", "Bob", 10)
+    tx2 = Transaction("Bob", "Charlie", 5)
+    tx3 = Transaction("Charlie", "Alice", 3)
 
-  blockchain.print_chain()
+    # add the transactions to a block and add the block to the blockchain
+    blockchain.add_block([tx1, tx2, tx3])
+
+    # print the blockchain to verify it was updated correctly
+    blockchain.print_chain()
+
+    # verify the blockchain is valid
+    print("Blockchain is valid: ", blockchain.is_valid())
+
+    # create an invalid transaction and add it to a block
+    invalid_tx = Transaction("Eve", "Mallory", -100)
+    blockchain.add_block([invalid_tx])
+
+    # print the blockchain to verify the invalid block was added
+    blockchain.print_chain()
+
+    # verify the blockchain is invalid
+    print("Blockchain is valid: ", blockchain.is_valid())
