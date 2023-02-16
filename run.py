fe71ef5b7d5a8326d572dc801dd2438de957a3d8
Utku Yücel <40201124+utkuyucel@users.noreply.github.com>
2023-02-16 13:47:42 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,106 +3,91 @@ import datetime
 import uuid
 
 class Transaction:
-    def __init__(self, sender, receiver, amount, fee=0, timestamp=None):
-        self.id = str(uuid.uuid4())
+    def __init__(self, sender, recipient, amount, fee, transaction_type=None):
         self.sender = sender
-        self.receiver = receiver
+        self.recipient = recipient
         self.amount = amount
         self.fee = fee
-        self.timestamp = timestamp if timestamp else datetime.datetime.now()
+        self.transaction_type = transaction_type
 
-    def is_valid(self):
-        if self.amount <= 0:
-            return False
-        return True
-
+    def __str__(self):
+        return f"Transaction(sender={self.sender}, recipient={self.recipient}, amount={self.amount}, fee={self.fee}, type={self.transaction_type})"
 
 class Block:
     def __init__(self, transactions, previous_hash):
-        self.timestamp = datetime.datetime.now()
         self.transactions = transactions
         self.previous_hash = previous_hash
+        self.timestamp = datetime.datetime.now()
         self.nonce = 0
-        self.hash = self.calc_hash()
+        self.hash = self.generate_hash()
 
-    def calc_hash(self):
-        sha = hashlib.sha256()
-
-        # Hash the block's transaction data, previous block's hash, and nonce
-        hash_str = str([t.__dict__ for t in self.transactions]).encode('utf-8') + \
-                   str(self.previous_hash).encode('utf-8') + str(self.nonce).encode('utf-8')
-
-        sha.update(hash_str)
-
-        return sha.hexdigest()
-
-    def mine_block(self, difficulty):
-        while self.hash[:difficulty] != "0" * difficulty:
-            self.nonce += 1
-            self.hash = self.calc_hash()
-
-        print("Block mined: ", self.hash)
-
-    def has_valid_transactions(self):
-        for transaction in self.transactions:
-            if not transaction.is_valid():
-                return False
-        return True
+    def generate_hash(self):
+        block_contents = str(self.previous_hash) + str(self.timestamp) + str(self.transactions) + str(self.nonce)
+        block_hash = hashlib.sha256(block_contents.encode())
+        return block_hash.hexdigest()
 
 class Blockchain:
     def __init__(self):
-        self.chain = [Block([], None)]
-        self.difficulty = 2
+        self.chain = []
         self.transaction_pool = []
 
     def add_transaction_to_pool(self, transaction):
         self.transaction_pool.append(transaction)
 
     def add_transactions_to_pool(self, transactions):
-        for transaction in transactions:
-            self.transaction_pool.append(transaction)
+        self.transaction_pool.extend(transactions)
 
-    def process_transaction_pool(self, reward):
-        transactions = self.transaction_pool.copy()
-        self.transaction_pool.clear()
+    def process_transaction_pool(self, reward, transaction_fee=0.01):
+        if not self.transaction_pool:
+            return
 
-        # Create a new transaction for the reward
-        reward_tx = Transaction("coinbase", reward, reward, timestamp=datetime.datetime.now())
-        transactions.append(reward_tx)
+        # Create a coinbase transaction as the first transaction in the block
+        coinbase_tx = Transaction("coinbase", "miner", reward, transaction_fee)
+        block_transactions = [coinbase_tx]
 
-        self.add_block(transactions)
+        # Add transactions from the pool to the block until the block is full
+        remaining_reward = reward
+        for tx in self.transaction_pool:
+            if len(block_transactions) >= 10:
+                break
 
-    def add_block(self, transactions):
-        previous_hash = self.chain[-1].hash
+            if remaining_reward >= tx.amount + tx.fee:
+                remaining_reward -= tx.amount + tx.fee
+                block_transactions.append(tx)
 
-        new_block = Block(transactions, previous_hash)
-        new_block.mine_block(self.difficulty)
+        # Create a new block and add it to the chain
+        previous_hash = self.chain[-1].hash if self.chain else None
+        new_block = Block(block_transactions, previous_hash)
         self.chain.append(new_block)
 
+        # Remove processed transactions from the pool
+        self.transaction_pool = [tx for tx in self.transaction_pool if tx not in block_transactions]
+
     def print_chain(self):
-        for block in self.chain:
-            print("Timestamp: ", block.timestamp)
-            print("Transactions: ", [t.__dict__ for t in block.transactions])
-            print("Previous Hash: ", block.previous_hash)
-            print("Hash: ", block.hash)
+        for i, block in enumerate(self.chain):
+            print(f"Block {i}:")
+            print(f"Timestamp: {block.timestamp}")
+            print(f"Previous hash: {block.previous_hash}")
+            print(f"Transactions: {block.transactions}")
+            print(f"Nonce: {block.nonce}")
+            print(f"Hash: {block.hash}")
             print()
 
     def is_valid(self):
+        if not self.chain:
+            return True
+
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i-1]
 
-            if current_block.hash != current_block.calc_hash():
-                return False
-
             if current_block.previous_hash != previous_block.hash:
                 return False
 
-            if not current_block.has_valid_transactions():
+            if current_block.hash != current_block.generate_hash():
                 return False
 
         return True
-
 
 if __name__ == "__main__":
     blockchain = Blockchain()
@@ -111,18 +96,21 @@ if __name__ == "__main__":
     reward = 1
 
     # create some transactions with fees and add them to the transaction pool
-    tx1 = Transaction("Alice", "Bob", 10, 0.1)
-    tx2 = Transaction("Bob", "Charlie", 5, 0.05)
-    tx3 = Transaction("Charlie", "Alice", 3, 0.03)
+    tx1 = Transaction("Alice", "Bob", 10, 0.1, "regular")
+    tx2 = Transaction("Bob", "Charlie", 5, 0.05, "regular")
+    tx3 = Transaction("Charlie", "Alice", 3, 0.03, "regular")
     blockchain.add_transactions_to_pool([tx1, tx2, tx3])
 
     # add more transactions to the pool
-    tx4 = Transaction("David", "Eva", 2, 0.02)
-    tx5 = Transaction("Eva", "Frank", 7, 0.07)
+    tx4 = Transaction("David", "Eva", 2, 0.02, "regular")
+    tx5 = Transaction("Eva", "Frank", 7, 0.07, "regular")
     blockchain.add_transaction_to_pool(tx4)
     blockchain.add_transaction_to_pool(tx5)
 
     # process the transaction pool to create a new block
+    blockchain.process_transaction_pool(reward)
+
+    tx6 = Transaction("Utku", "Kna", 5, 0.01, "regular")
     blockchain.process_transaction_pool(reward)
 
     # print the blockchain
@@ -130,3 +118,5 @@ if __name__ == "__main__":
 
     # check if the blockchain is valid
     print(blockchain.is_valid())
+
+
